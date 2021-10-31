03528e0ca87810c85243cbd25a3e9585f6794c20
Your Name <you@example.com>
2021-10-31 19:26:34 +0300

    add new class BasePageLocators in locators.py

--- test_product_page.py
+++ test_product_page.py
@@ -27,36 +27,48 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 # xlink = pytest.param(mask+str(xfile), marks=pytest.mark.xfail(reason="mistake on page"))
 # links.insert(xfile, xlink)
 
-@pytest.mark.parametrize('link', links)
-def test_guest_can_add_product_to_basket(browser, link):
-    # link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{link}"
-    page = ProductPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()                      # открываем страницу
-    page.go_to_basket_page()        # нажать на кнопку "add to basket"
-    time.sleep(50)
-    page.should_solve_quiz_and_get_code()   # решаем quiz
-    time.sleep(50)
-    page.should_be_correct_added_product()  # проверяем корректность цены и названия продукта
+# @pytest.mark.parametrize('link', links)
+# def test_guest_can_add_product_to_basket(browser, link):
+#     # link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer{link}"
+#     page = ProductPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+#     page.open()                      # открываем страницу
+#     page.go_to_basket_page()        # нажать на кнопку "add to basket"
+#     time.sleep(50)
+#     page.should_solve_quiz_and_get_code()   # решаем quiz
+#     time.sleep(50)
+#     page.should_be_correct_added_product()  # проверяем корректность цены и названия продукта
+#
+# def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+#     # link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+#     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+#     page = ProductPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+#     page.open()                      # открываем страницу
+#     page.go_to_basket_page()        # нажать на кнопку "add to basket"
+#     page.should_solve_quiz_and_get_code()   # решаем quiz
+#     page.should_not_be_success_message()
+#
+# def test_guest_cant_see_success_message(browser):
+#     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+#     page = ProductPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+#     page.open()                      # открываем страницу
+#     page.should_not_be_success_message()
+#
+# def test_message_disappeared_after_adding_product_to_basket(browser):
+#     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
+#     page = ProductPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+#     page.open()                      # открываем страницу
+#     page.go_to_basket_page()        # нажать на кнопку "add to basket"
+#     page.should_solve_quiz_and_get_code()   # решаем quiz
+#     page.should_dissapear_of_success_message()
 
-def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
-    # link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
-    page = ProductPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()                      # открываем страницу
-    page.go_to_basket_page()        # нажать на кнопку "add to basket"
-    page.should_solve_quiz_and_get_code()   # решаем quiz
-    page.should_not_be_success_message()
+def test_guest_should_see_login_link_on_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
 
-def test_guest_cant_see_success_message(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
-    page = ProductPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()                      # открываем страницу
-    page.should_not_be_success_message()
-
-def test_message_disappeared_after_adding_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019"
-    page = ProductPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
-    page.open()                      # открываем страницу
-    page.go_to_basket_page()        # нажать на кнопку "add to basket"
-    page.should_solve_quiz_and_get_code()   # решаем quiz
-    page.should_dissapear_of_success_message()
+def test_guest_can_go_to_login_page_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
